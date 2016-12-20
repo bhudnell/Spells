3ded6d22c0e15d445745181f457671ec0036ecd6
bhudnell1240 <bthudnell@gmail.com>
2016-12-20 00:49:50 -0700

    Added initial files

---
+++ Spells GUI/src/backend/controller.java
@@ -0,0 +1,11 @@
+package backend;
+
+public class controller {
+
+	public static void main(String[] args) {
+		DatabaseAdaptor test = new DatabaseAdaptor();
+		
+		test.nameSearch("summon");
+	}
+
+}
